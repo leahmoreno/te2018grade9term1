fa353d4605e031020dce2cc325e7857965570a6a
leahmoreno <leah.moreno@csedge.org>
2018-10-06 15:47:29 -0400

    completed challenge 1
    \

--- data.js
+++ data.js
@@ -1,5 +1,30 @@
 // Example
 let message = "Hello World!";
 console.log(message);
+let lovesFood;
+let hasADog;
+let hatesToSing;
+let birthday= "My birthday is"
+let sleep = "4"
+let num1;
+let num2 =1;
+let num3 =3;
+console.log(birthday)
+console.log(sleep)
+let eat = "27"
+console.log(eat)
+let sing = "04"
+console.log(sing)
+hasADog= true
+console.log(hasADog)
+hatesToSing= false
+console.log(hatesToSing)
+lovesFood= true
+console.log(lovesFood)
+const READLINE=require("readline-sync")
+let userInput=READLINE.question('Type anything and ill repeat it back!')
+console.log(userInput);
+
+
 
 // Type your code below!

6cf4380492026e0f70e21b9449c6eb47aa403ef6
leahmoreno <leah.moreno@csedge.org>
2018-10-13 14:16:07 -0400

    easter challenge finsihed

--- math.js
+++ math.js
@@ -1,6 +1,6 @@
 // Variables
-let num1 = 5;
-let num2 = 10;
+let num1 = 21;
+let num2 = 2;
 
 // **** Problem 1 ****
 // Store the sum of num1 and num2 in a variable.
@@ -11,27 +11,33 @@ console.log(sum);
 // **** Problem 2 ****
 // Store the difference between sum (above) and 7 in a variable.
 // Print the difference.
-
+let difference = sum - 7;
+console.log(difference);
 
 // **** Problem 3 ****
 // Store the product of difference and 3 in a variable.
 // Print the product.
+let product = difference * 3;
+console.log(product);
 
 
 // **** Problem 4 ****
 // Store the power of the product raised to an exponent of 2 in a variable.
 // Print the power.
-
+let power = product ** 2;
+console.log(power);
 
 // **** Problem 5 ****
 // Store the quotient of the power divided by 4 in a variable.
 // Print the quotient.
-
+let quotient = power/4;
+console.log(quotient)
 
 // **** Problem 6 ****
 // Store the remainder of the quotient divided by 2 in a variable.
 // Print the remainder.
-
+let remainder = quotient/2
+console.log(remainder)
 
 // **** Problem 7 ****
 // Print a boolean value (true/false) representing if the quotient from

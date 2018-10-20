b39da349093e03ffc4c7ef424dbb76fe8ddb2353
leahmoreno <leah.moreno@csedge.org>
2018-10-20 12:51:48 -0400

    finsihed

---
+++ objects.js
@@ -0,0 +1,22 @@
+let villain = {
+	firstName: "Captain",
+	isVillain:  true,
+	lastName:  "Underpants",
+	catchphrase:"i dont care",
+	worstCrime: "once took candy from a baby"
+};
+let hacker = {
+	spyFirst: "Speedy",
+	isHacker: true,
+	spyLast:"Leah",
+	spyCatchphrase:"Nobody can catch me",
+	archenemy:"Captain Underpants"
+};
+console.log (`His name is ${villain.firstName} ${villain.lastName}`);
+console.log (`His catchphrase is ${villain.catchphrase} and his worst crime was that he ${villain.worstCrime}`);
+console.log (`My name is ${hacker.spyFirst} ${hacker.spyLast}`);
+console.log (`My catchphrase is that ${hacker.spyCatchphrase} and my archenemy is ${hacker.archenemy}`);
+
+
+
+
